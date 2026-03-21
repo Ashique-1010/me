18741543c2436ac0bd17849f9a37bcbf0739e657
Ashique A B <ab.ashique10@gmail.com>
2026-03-22 00:09:45 +0530

    deployment changes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  trailingSlash: true,
+  assetPrefix: isProd ? '/me/' : '',
+  basePath: isProd ? '/me' : '',
+  output: 'export',
 };
 
+module.exports = nextConfig
 export default nextConfig;
