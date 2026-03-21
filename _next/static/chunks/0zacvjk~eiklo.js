afd5665281b104394d0ddaf9857e4a2cc3f72e6f
Ashique A B <ab.ashique10@gmail.com>
2026-03-22 00:53:22 +0530

    Updates

--- _next/static/chunks/0zacvjk~eiklo.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/devHomePage", [
-  "static/chunks/0~jnwa_h400y1.js",
-  "static/chunks/00qk2ee6l5kfn.js",
-  "static/chunks/turbopack-0od7xicqiu_j-.js"
-])
--- _next/static/chunks/0zki72u5bc21z.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/homePage", [
-  "static/chunks/0ch6dzfxy6ndt.js",
-  "static/chunks/00qk2ee6l5kfn.js",
-  "static/chunks/turbopack-037kdq.vt69_t.js"
-])
